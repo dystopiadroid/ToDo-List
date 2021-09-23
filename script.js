17d2e38e964939faf401c0698f22902781b2380e
Bala <bpjdev25@gmail.com>
2021-09-23 15:44:30 +0530

    deleted script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// window.addTodo(event) {
-//     event.preventDefault()
-//     let todo = document.getElementById('create-todo-input')
-//     console.log(event)
-//     db.collection('items-list').add({
-//         status : "active",
-//         text : todo.value
-//     })
-// }
